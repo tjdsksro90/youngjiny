68a91de5dff3d7d23f4eac8504b2b0f8a241204e
jjuhee <azaza.hee@gmail.com>
2023-10-28 16:08:18 +0900

    feat: 현재상영중 추가사항
    1) 현재 날짜와 이주전 날짜 생성해서 쿼리 넘겨주기 추가
    2) css 추가, display flex 적용
    3) 각페이지에 현재상영중 링크 삽입

--- js/nowplay.js
+++ js/nowplay.js
@@ -1,3 +1,5 @@
+// import { handleSearch } from "./search.js";
+
 const options = {
   method: "GET",
   headers: {
@@ -7,34 +9,43 @@ const options = {
   }
 };
 
-//language : ko-KR (한국어) , region : KR (남한) : //URL을 바꿔가면서 수정해하나?
-// const fetchNowPlayMovieData = async (page) => {
-//   const { results } = await fetch(
-//     `https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&&page=${page}&region=KR`,
-//     options
-//   )
-//     .then((response) => response.json())
-//     .then((response) => {
-//       totalPages = response.total_pages;
-//       console.log("jhee2:", response);
-//       return response;
-//     })
-//     .catch((err) => console.error(err));
+// yyyy-mm-dd 로 만들어주기
+const getTodayDateString = (day) => {
+  let year = day.getFullYear(); // 2003
+  let month = day.getMonth() + 1; // 0 ~ 11
+  let date = day.getDate(); //1 ~ 31
 
-//   return results;
-// };
+  if (month < 10) {
+    month = "0" + String(month);
+  }
+  if (date < 10) {
+    date = "0" + String(date);
+  }
+
+  let dateString = year + "-" + month + "-" + date;
+  return dateString;
+};
+
+// 2주전 날짜랑 오늘 날짜 배열
+const get2WeeksAgo = () => {
+  let today = new Date();
+  let weeksAgo = new Date(new Date().setDate(1 - 14));
+
+  return [getTodayDateString(weeksAgo), getTodayDateString(today)];
+};
 
 // TMDB에는 실제 상영 여부 판단 안되고 최근 개봉일 (약 2주전) 구간으로 판단함
-// jhee TODO : 날짜 구간 전달 해야함... ㄷㄷ
-const fetchNowPlayTEST = async (page, sort) => {
+// 국내 상영만 보여주기 language : ko-KR (한국어) , region : KR (남한)
+const fetchNowPlaying = async (page, sort) => {
+  let arrayDays = get2WeeksAgo();
+  let dates = `&release_date.gte=${arrayDays[0]}&release_date.lte=${arrayDays[1]}`;
+
   const { results } = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&&region=KR&page=${page}&sort_by=${sort}&with_release_type=2|3&release_date.gte=2023-09-14&release_date.lte=2023-11-01
-    `,
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&&region=KR&page=${page}&sort_by=${sort}&with_release_type=2|3${dates}`,
     options
   )
     .then((response) => response.json())
     .then((response) => {
-      console.log("객체:", response);
       totalPages = response.total_pages;
       return response;
     })
@@ -45,11 +56,9 @@ const fetchNowPlayTEST = async (page, sort) => {
   return results;
 };
 
-// 영화 정보의 배열을 순회하며 영화 카드를 만드는 함수 : //재사용 가능여부 확인 필요
+// 영화 정보의 배열을 순회하며 영화 카드를 만드는 함수 :
 const makeNodwMovieCards = async (page, sort) => {
-  //const movies = await fetchNowPlayMovieData(page);
-  const movies = await fetchNowPlayTEST(page, sort);
-  console.log("jhee :", movies);
+  const movies = await fetchNowPlaying(page, sort);
   const cardList = document.querySelector(".card-list");
   let tempHTML = movies
     .map((movie) => {
@@ -62,7 +71,7 @@ const makeNodwMovieCards = async (page, sort) => {
               <!-- <p class="movie-overview">${overview}</p> -->
               <p class="movie-overview">인기점수 : ${popularity} </p>
               <p class="movie-release">개봉일 : ${release_date} </p>
-              <p class="movie-rating">${vote_average} / <span>10</span>
+              <p class="movie-rating"> <span>${vote_average.toFixed(1)}</span> / 10
               </p>
             </div>
           </article>`;
@@ -75,53 +84,34 @@ const makeNodwMovieCards = async (page, sort) => {
   cardList.innerHTML = cardList.innerHTML.concat(tempHTML);
 };
 
-const getTimeString = (today) => {
-  //let date = today.toLocaleDateString();
-  let date = today.toLocaleDateString().replaceAll(".", "").replaceAll(" ", "-");
-  console.log("jhee", date);
-  return date;
-};
-
 // 시작!
 const moreBtn = document.querySelector(".button-more-item"); // 더보기 버튼
 const serchBar = document.querySelector("#nav-search"); // 서치바 가림
 const sortedBy = document.querySelector("#sorted-by"); //select
-let today = new Date();
-let page = 1; //page를 node 속성으로 넣는 것도 고려해보자.
-let sortedByThis = "popularity.desc"; //얘도.
-let totalPages = 0;
+let page = 1; // page를 전역 변수로 가져가야겠니? node 에 넣기?
+let totalPages = 0; // 얘는.. fetch 할때 넣어준다.
 
-getTimeString(today);
-makeNodwMovieCards(page, sortedByThis);
+makeNodwMovieCards(page, sortedBy.value);
 
 // 더 보기 버튼을 누르면 다음 페이지를 로드하고 마지막 페이지이면 더보기 버튼을 가린다
-const onMoreBtnClicked = (e) => {
-  console.log("page :", ++page, "sorted by : ", sortedByThis);
-  makeNodwMovieCards(page, sortedByThis);
+const onMoreBtnClicked = () => {
+  makeNodwMovieCards(++page, sortedBy.value);
   if (page === totalPages) moreBtn.classList.add("invisible");
 };
 moreBtn.addEventListener("click", onMoreBtnClicked);
 
-//서치바 가림
-serchBar.style.display = "none";
-
-//소트 리스너: 변경되면 value 가져오기
+//소트 리스너: 변경되면 value 가져오기 /  페이지 초기화 및 더보기 생성
 sortedBy.addEventListener("change", (e) => {
-  console.log("jhee1:", e.target.value, "current:", e.currentTarget, "value? ", e.currentTarget.value);
-  // 페이지 초기화 및 더보기 생성 되는걸까?
   page = 1;
   moreBtn.classList.remove("invisible");
-  sortedByThis = e.target.value;
-  makeNodwMovieCards(page, sortedByThis);
+  makeNodwMovieCards(page, e.target.value);
 });
 
 const selectQuery = (form) => {
   return form;
 };
 
-// 스크롤 바닥 확인하면 로드 하고 싶다
-// window.scroll(function () {
-//   if (window.scrollTop() + window.height() == document.height()) {
-//     alert("bottom!");
-//   }
-// });
+//서치바 가림
+serchBar.style.display = "none";
+// const form = document.querySelector("#nav-search");
+// form.addEventListener("submit", handleSearch);
