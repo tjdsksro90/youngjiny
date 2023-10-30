3233e02c4cda91273b17d0e673edbb3a21f82d77
gilhun <qkrrlfgns123@gmail.com>
2023-10-30 16:54:10 +0900

    드라마 작업 마무리

--- js/onTheAir.js
+++ js/onTheAir.js
@@ -1,3 +1,5 @@
+import { onDarkmodeBtnClicked } from "./utils.js";
+
 const submitBtn = document.getElementById("search-btn");
 
 const options = {
@@ -8,7 +10,7 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmEyYzIzMWRjMTQxMGEzNjk3ZWEzOWQyMjY2M2IwZiIsInN1YiI6IjY1MzBlZmMzNTFhNjRlMDBjOGZkY2I5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jYFT5VOAZYR9SPBGGB16_GUZgiU7Bmkvz6G-5Qwiw48"
   }
 };
-fetch("https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=1", options)
+fetch("https://api.themoviedb.org/3/tv/on_the_air?language=ko-US&page=1", options)
   .then((response) => response.json())
   .then((response) => {
     createOnAirCard(response);
@@ -36,7 +38,7 @@ const createOnAirCard = (onAirs) => {
       voteAverageElement.classList.add("vote-average");
       image.setAttribute("src", `https://image.tmdb.org/t/p/w500/${poster_path}`);
       nameElement.textContent = name;
-      overviewElement.textContent = overview;
+      overviewElement.textContent = overview ? overview : "줄거리가 없습니다.";
       voteAverageElement.innerHTML = `<span>${vote_average}</span> / 10`;
       card.setAttribute("id", id);
       card.addEventListener("click", (e) => {
@@ -46,7 +48,7 @@ const createOnAirCard = (onAirs) => {
           media: "tv"
         };
         location.href = "detail.html";
-        sessionStorage.setItem("data", JSON.stringify(data))
+        sessionStorage.setItem("data", JSON.stringify(data));
       });
       card.appendChild(image);
       card.appendChild(nameElement);
@@ -55,36 +57,77 @@ const createOnAirCard = (onAirs) => {
       onAirContainer.appendChild(card);
     });
 };
-const onSearchClick1 = (e) => {
-  e.preventDefault();
-  const input = document.getElementById("search-input");
-  const value = input.value;
-  console.log(value);
-  filterOnAir(value);
-};
-submitBtn.addEventListener("click", onSearchClick1);
-const filterOnAir = function (value) {
-  let deleteCard = [];
-  let showCard = [];
-  let cards = Array.from(document.querySelectorAll(".name"));
-  showCard = cards.filter((card) => {
-    let name = card.innerHTML;
-    name = name.toLowerCase();
-    if (name.includes(value.toLowerCase())) {
-      return card;
-    } else {
-      deleteCard.push(card);
-    }
-  });
-  console.log(showCard);
-  console.log(deleteCard);
-  deleteCard.forEach((card) => {
-    card.closest(".onAir-card").style.display = "none";
-  });
-};
+
 const renderOnAir = (onAirs) => {
   onAirs.forEach((onAir) => {
     const onAirCard = createOnAirCard(onAir);
     onAirContainer.appendChild(onAirCard);
+  });
+};
+
+//const submitBtn = document.getElementById("search-btn");
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmEyYzIzMWRjMTQxMGEzNjk3ZWEzOWQyMjY2M2IwZiIsInN1YiI6IjY1MzBlZmMzNTFhNjRlMDBjOGZkY2I5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jYFT5VOAZYR9SPBGGB16_GUZgiU7Bmkvz6G-5Qwiw48",
+//   },
+// };
+fetch("https://api.themoviedb.org/3/tv/top_rated?language=ko-US&page=1", options)
+  .then((response) => response.json())
+  .then((response) => {
+    createTopRateCard(response);
+    console.log("fetch TopRate");
+  })
+  .catch((err) => console.error(err));
+const topRateContainer = document.getElementById("topRate-container");
+const createTopRateCard = (topRates) => {
+  topRates.results
+    .filter((topRate, i) => {
+      return true;
+    })
+    .forEach((topRate) => {
+      const { id, name, overview, poster_path, vote_average } = topRate;
+      const card = document.createElement("div");
+      const image = document.createElement("img");
+      const nameElement = document.createElement("div");
+      const overviewElement = document.createElement("div");
+      const voteAverageElement = document.createElement("div");
+      card.classList.add("topRate-card");
+
+      image.classList.add("poster-image");
+      nameElement.classList.add("name");
+      overviewElement.classList.add("overview");
+      voteAverageElement.classList.add("vote-average");
+      image.setAttribute("src", `https://image.tmdb.org/t/p/w500/${poster_path}`);
+      nameElement.textContent = name;
+      overviewElement.textContent = overview ? overview : "줄거리가 없습니다.";
+      voteAverageElement.innerHTML = `<span>${vote_average}</span> / 10`;
+      card.setAttribute("id", id);
+      card.addEventListener("click", (e) => {
+        const id = e.currentTarget.getAttribute("id");
+        const data = {
+          id: id,
+          media: "tv"
+        };
+        location.href = "detail.html";
+        sessionStorage.setItem("data", JSON.stringify(data));
+      });
+      card.appendChild(image);
+      card.appendChild(nameElement);
+      card.appendChild(overviewElement);
+      card.appendChild(voteAverageElement);
+      topRateContainer.appendChild(card);
+    });
+};
+
+const darkmodeBtn = document.querySelector(".button-darkmode");
+darkmodeBtn.addEventListener("click", onDarkmodeBtnClicked);
+
+const renderTopRate = (topRates) => {
+  topRates.forEach((topRate) => {
+    const topRateCard = createTopRateCard(topRate);
+    topRateContainer.appendChild(topRateCard);
   });
 };
